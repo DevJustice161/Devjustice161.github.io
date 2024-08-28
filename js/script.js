2f09f14a7300c7b5c4362625e873b8f5abeadf57
Justice <107154661+DevJustice161@users.noreply.github.com>
2024-08-28 14:22:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
     const notification = document.getElementById("notification");
+    const downloadBtn = document.getElementById("downloadBtn");
 
     downloadBtn.addEventListener("click", function() {
         // Show notification
         notification.style.display = "block";
+        notification.style.color = "red";
 
         // Hide notification after 3 seconds
         setTimeout(function() {
